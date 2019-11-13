25502ba6ca27f583b734df70ab752852919f5139
liuchao <974854469@qq.com>
2019-11-13 18:06:42 +0800

    Spring Cloud Eureka：服务注册与发现

---
+++ eureka-security-server/src/main/java/com/liucccc/cloud/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.liucccc.cloud.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * WebSecurityConfig
+ *
+ * @author liuchao
+ * @date 2019-11-13 17:45
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
